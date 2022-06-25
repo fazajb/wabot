e974148b5d4ed77525348756833f528c0182d57f
fazajb <108041486+fazajb@users.noreply.github.com>
2022-06-25 12:00:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['6289647417373'] // nomer anda
+global.owner = ['6285803688993'] // nomer anda
 global.mods = [] // Want some help?
-global.prems = ['6289999999999'] // Premium user has unlimited limit
+global.prems = ['6285803688993'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -21,7 +21,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Wabot-aq02'
+global.author = 'FazaXd'
 
 global.multiplier = 69 // The higher, The harder levelup
 
